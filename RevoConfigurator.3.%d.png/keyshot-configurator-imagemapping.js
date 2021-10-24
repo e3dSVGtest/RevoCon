e5a157114e4c04771fb3062efaeca49ffa98f94e
e3dSVGtest <67582565+e3dSVGtest@users.noreply.github.com>
2021-10-24 11:53:31 +0100

    Initial Upload

---
+++ RevoConfigurator.3.%d.png/keyshot-configurator-imagemapping.js
@@ -0,0 +1,21 @@
+imagemappingdata =
+[
+    {
+        "image": "RevoConfigurator.3.1.png",
+        "modelsets": [
+            1299005029
+        ]
+    },
+    {
+        "image": "RevoConfigurator.3.2.png",
+        "modelsets": [
+            1298973842
+        ]
+    },
+    {
+        "image": "RevoConfigurator.3.3.png",
+        "modelsets": [
+            1298953280
+        ]
+    }
+];
---
+++ RevoConfigurator.3.%d.png/keyshot-configurator-modeldata.js
@@ -0,0 +1,23 @@
+model =
+{
+    "materialgroups": [],
+    "products": [
+        {
+            "icon": "revo%20hemera.png",
+            "modelset": 1299005029,
+            "name": "Revo Hemera"
+        },
+        {
+            "checked": true,
+            "icon": "revo%20six.png",
+            "modelset": 1298973842,
+            "name": "Revo Six"
+        },
+        {
+            "icon": "revo%20micro.png",
+            "modelset": 1298953280,
+            "name": "Revo Micro"
+        }
+    ],
+    "studios": []
+};
